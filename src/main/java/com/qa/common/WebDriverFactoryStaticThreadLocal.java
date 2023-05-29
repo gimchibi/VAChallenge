8d81f1133edf37f920fe70d815000a5527f3b2a8
enggi <gimchibi@gmail.com>
2023-05-29 18:34:18 +0800

    update framework

--- src/main/java/com/qa/common/WebDriverFactoryStaticThreadLocal.java
+++ src/main/java/com/qa/common/WebDriverFactoryStaticThreadLocal.java
@@ -9,20 +9,6 @@ import java.util.HashMap;
 
 public class WebDriverFactoryStaticThreadLocal {
 
-  //  public static ChromeDriver driver;
-
-/*    public static WebDriver getdriver() {
-        if (driver == null) {
-            String downloadDir = System.getProperty("user.dir") + "\\src\\test\\resources\\DownloadDirectory";
-            HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
-            chromePrefs.put("profile.default_content_settings.popups", 0);
-            chromePrefs.put("download.default_directory", downloadDir);
-            ChromeOptions options = new ChromeOptions();
-            options.setExperimentalOption("prefs", chromePrefs);
-            driver = new ChromeDriver(options);
-        }
-        return driver;
-    }*/
     private static ThreadLocal<WebDriver> driver = new ThreadLocal<WebDriver>();
 
     public static void setDriver() {
